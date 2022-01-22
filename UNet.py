7274c6064e404647775568300f7dd8433a07d14c
Gustavo Scholze <scholzegustavo@gmail.com>
2022-01-21 21:55:38 -0300

    added resnet decoder blocks

--- UNet.py
+++ UNet.py
@@ -93,10 +93,39 @@ class UNet(nn.Module):
         
         return out
 
-
+nonlinearity = partial(F.relu, inplace=True)
 
 class Decoder(nn.Module):
-    pass
+    def __init__(self, in_channels, n_filters):
+        super(Decoder, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
+        self.norm1 = nn.BatchNorm2d(in_channels // 4)
+        self.relu1 = nonlinearity
+
+        self.deconv = nn.ConvTranspose2d(in_channels // 4, in_channels // 4, 3,
+                stride=2, padding=1, output_padding=1)
+        self.norm2 = nn.BatchNorm2d(in_channels // 4)
+        self.relu2 = nonlinearity
+
+        self.conv2 = nn.Conv2d(in_channels // 4, n_filters, 1)
+        self.norm3 = nn.BatchNorm2d(n_filters)
+        self.relu3 = nonlinearity
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu1(x)
+
+        x = self.deconv(x)
+        x = self.norm2(x)
+        x = self.relu2(x)
+
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.relu3(x)
+
+        return x
 
 class ResNet34_UNet(nn.Module):
     pass
