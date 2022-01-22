b2fa3dcfe155b29c7bffea2b031ab768e5fd279b
Gustavo Scholze <scholzegustavo@gmail.com>
2022-01-21 21:39:50 -0300

    forward pass unet

--- UNet.py
+++ UNet.py
@@ -22,7 +22,78 @@ class DoubleConv(nn.Module):
 
 
 class UNet(nn.Module):
-    pass
+    def __init__(self, in_ch, out_ch):
+        super(UNet, self).__init__()
+
+        # Double Conv Layers => Pool() and in the end, => Up-Conv
+        self.conv1 = DoubleConv(in_ch, 32)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.conv2 = DoubleConv(32, 64)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.conv3 = DoubleConv(64, 128)
+        self.pool3 = nn.MaxPool2d(2)
+
+        self.conv4 = DoubleConv(128, 256)
+        self.pool4 = nn.MaxPool2d(2)
+
+        # Up-Conv stage
+        self.conv5 = DoubleConv(256, 512)
+        self.up1 = nn.ConvTranspose2d(512, 256, 2, stride=2)
+
+        self.conv6 = DoubleConv(512, 256)
+        self.up2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+
+        self.conv7 = DoubleConv(256, 128)
+        self.up3 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+
+        self.conv8 = DoubleConv(128, 64)
+        self.up4 = nn.ConvTranspose2d(64, 32, 2, stride=2)
+
+        self.conv9 = DoubleConv(64, 32)
+        self.conv10 = nn.Conv2d(32, out_ch, 1)
+
+    def forward(self, x):
+        c1 = self.conv1(x)
+        p1 = self.pool1(c1)
+
+        c2 = self.conv2(p1)
+        p2 = self.pool2(c2)
+
+        c3 = self.conv3(p2)
+        p3 = self.pool3(c3)
+
+        c4 = self.conv4(p3)
+        p4 = self.pool4(c4)
+
+        c5 = self.conv5(p4)
+        up1 = self.up1(c5)
+
+        merge1 = torch.cat([up1, c4], dim=1)
+
+        c6 = self.conv6(merge1)
+        up2 = self.up2(c6)
+        
+        merge2 = torch.cat([up2, c3], dim=1)
+
+        c7 = self.conv7(merge2)
+        up3 = self.up3(c7)
+
+        merge3 = torch.cat([up3, c2], dim=1)
+
+        c8 = self.conv8(merge3)
+        up4 = self.up4(c8)
+
+        merge4 = torch.cat([up4, c1], dim=1)
+
+        c9 = self.conv9(merge4)
+        c10 = self.conv10(c9)
+        out = nn.Sigmoid()(c10)
+        
+        return out
+
+
 
 class Decoder(nn.Module):
     pass
