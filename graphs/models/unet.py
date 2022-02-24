adaed2c53b3248dc248f89786b19342287196b37
Gustavo Scholze <scholzegustavo@gmail.com>
2022-02-24 19:31:03 -0300

    setting up

--- graphs/models/unet.py
+++ graphs/models/unet.py
@@ -1,2 +1,26 @@
-import segmentation_models_pytorch as smp
-model = smp.Unet('mobilenet_v2', encoder_weights='imagenet', classes=23, activation=None, encoder_depth=5, decoder_channels=[256, 128, 64, 32, 16])
+"""
+An example for the model class
+"""
+import torch.nn as nn
+
+from graphs.weights_initializer import weights_init
+
+class UNet(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        # define layers
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv = nn.Conv2d(in_channels=self.config.input_channels, out_channels=self.config.num_filters, kernel_size=3, stride=1, padding=1, bias=False)
+
+        # initialize weights
+        self.apply(weights_init)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.relu(x)
+
+        out = x.view(x.size(0), -1)
+        return out
