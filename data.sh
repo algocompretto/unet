958c3d52ef2925c286f3b4e9b8ddd1fe68be0a83
algocompretto <scholzegustavo@gmail.com>
2022-12-07 07:17:42 -0300

    Downloading data from Kaggle CLI

---
+++ data.sh
@@ -0,0 +1,6 @@
+pip3 install --user kaggle
+kaggle competitions download -c tgs-salt-identification-challenge
+zip tgs-salt-identification-challenge .
+mkdir data/
+zip tgs-salt-identification-challenge/train.zip data/
+
